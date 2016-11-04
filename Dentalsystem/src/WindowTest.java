aba95e229108940f2fa6639bd8223463ff1be3a3
Anandee Karia <akaria2@sheffield.ac.uk>
2016-11-04 00:08:26 +0000

    updated gui - added white bar at top , neeed to sort out where the nav bar is

--- Dentalsystem/src/WindowTest.java
+++ Dentalsystem/src/WindowTest.java
@@ -10,6 +10,7 @@ public class WindowTest extends JFrame {
 
     private JPanel contentPane;
     private JPanel topMenu;
+    private JPanel header;
 
 
     public WindowTest(int w, int h) {
@@ -17,7 +18,7 @@ public class WindowTest extends JFrame {
         // Colours we'll need to paint the UI (RGB format)
         Color lightBlue = new Color(200, 200, 255);
         Color bgBlue = new Color(175, 175, 255);
-        Color white = new Color(255,255,255, 0);
+        Color white = new Color(255,255,255, 20);
 
 
 
@@ -25,58 +26,61 @@ public class WindowTest extends JFrame {
 
         JPanel contentPane = new JPanel(new GridBagLayout());
         contentPane.setPreferredSize(new Dimension(w, h));
-        contentPane.setBackground(bgBlue);
+        contentPane.setBackground(lightBlue);
         GridBagConstraints c = new GridBagConstraints(); // For managing UI
 
 
         // General configs and properties
         // TBC
-        /*
-        Box blank = new Box(1);
-        blank.setSize(w,50);
-        blank.setPreferredSize(new Dimension(w, 50));
-        blank.setBackground(white);
-        */
 
+        //header bit for logo
+        JPanel header = new JPanel();
+        header.setPreferredSize(new Dimension(w,140));
+        header.setBackground(Color.white);
 
         // Top Menu Stuff Here
         JPanel topMenu = new JPanel();
-        topMenu.setBackground(Color.black);
+        topMenu.setPreferredSize(new Dimension(w, 60));
+        topMenu.setBackground(bgBlue);
 
-        /*
+
+
         JButton menuItem  = new JButton("Home"); // Add action listeners later
         menuItem.setBackground(white);
         topMenu.add(menuItem);
         topMenu.setLayout(new GridLayout(1,2,7,10));
+
         menuItem = new JButton("Appointments");
         menuItem.setBackground(lightBlue);
         topMenu.add(menuItem);
+
         menuItem = new JButton("Health Care Plan");
         menuItem.setBackground(lightBlue);
         topMenu.add(menuItem);
+
         menuItem = new JButton("Patients");
         menuItem.setBackground(lightBlue);
         topMenu.add(menuItem);
+
         menuItem = new JButton("Contact");
         menuItem.setBackground(lightBlue);
         topMenu.add(menuItem);
-        menuItem.setOpaque(false 50%);
-        menuItem.setContentAreaFilled(false);
-        menuItem.setBorderPainted(false);
 
 
-*/
+
+
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         c.anchor = GridBagConstraints.FIRST_LINE_START;
+        c.fill = GridBagConstraints.HORIZONTAL;
         c.gridy = 0;
         c.gridx = 0;
-        c.gridheight = (int) 0.3*h;
-        c.gridwidth  = w;
         c.weightx = 1.0;
         c.weighty = 0.3;
-        c.fill = GridBagConstraints.HORIZONTAL;
-        contentPane.add(topMenu, c);
 
+        contentPane.add(header, c);
+
+        c.gridy = 1;
+        contentPane.add(topMenu, c);
 
 
         this.add(contentPane);
